76439134e1a0c39fe12a8146bca4ab755cc41571
victor.barbulescu23 <victor.barbulesu23@d155.org>
2022-08-22 08:33:49 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var sum: Double
+    var diff: Double
+    var product: Double
+    var quotient: Double
+    var mod: Double
+
+    var x = 5.0
+    var y = 2.0
+
+    sum = x + y
+    diff = x - y
+    product = x * y
+    quotient = (x/y)
+    mod = x%y
+
+    println("$x + $y = $sum")
+    println("$x - $y = $diff")
+    println("$x * $y = $product")
+    println("$x / $y = $quotient")
+    println("$x % $y = $mod")
+
+}
